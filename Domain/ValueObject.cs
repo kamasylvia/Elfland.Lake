30b6396a8c8dceec11cfca541f99805c2ad0f7c9
Yue Zhang <zhangyue.dev@outlook.com>
2022-05-16 20:04:03 +0800

    Add DDD code

--- Domain/ValueObject.cs
+++ Domain/ValueObject.cs
@@ -2,40 +2,30 @@ namespace Elfland.Lake.Domain;
 
 public abstract class ValueObject
 {
-    public static bool operator ==(ValueObject one, ValueObject two) => EqualOperator(one, two);
-
-    public static bool operator !=(ValueObject one, ValueObject two) => NotEqualOperator(one, two);
-
-    protected static bool EqualOperator(ValueObject left, ValueObject right)
-    {
-        if (ReferenceEquals(left, null) ^ ReferenceEquals(right, null))
-        {
-            return false;
-        }
-        return ReferenceEquals(left, null) || left.Equals(right);
-    }
+    protected static bool EqualOperator(ValueObject left, ValueObject right) =>
+        !(left is null ^ right is null) && (left is null || left.Equals(right!));
 
     protected static bool NotEqualOperator(ValueObject left, ValueObject right) =>
-        !(EqualOperator(left, right));
+        !EqualOperator(left, right);
 
     protected abstract IEnumerable<object> GetEqualityComponents();
 
     public override bool Equals(object? obj)
     {
-        if (obj == null || obj.GetType() != GetType())
+        if (obj is null || obj.GetType() != GetType())
         {
             return false;
         }
 
-        var other = (ValueObject)obj;
+        var other = obj as ValueObject;
 
-        return this.GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+        return GetEqualityComponents().SequenceEqual(other!.GetEqualityComponents());
     }
 
     public override int GetHashCode() =>
         GetEqualityComponents()
-            .Select(x => x != null ? x.GetHashCode() : 0)
+            .Select(x => x is not null ? x.GetHashCode() : 0)
             .Aggregate((x, y) => x ^ y);
 
-    public ValueObject? GetCopy() => this.MemberwiseClone() as ValueObject;
+    public ValueObject? GetCopy() => MemberwiseClone() as ValueObject;
 }
